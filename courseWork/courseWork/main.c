be24644f0556cd61c1c0c353dc7396c60f0cde7a
Mikhail Ostashchenko <68986785+Mikeost@users.noreply.github.com>
2021-05-07 15:18:14 +0300

    Update main.c
    Edit main.c

--- courseWork/courseWork/main.c
+++ courseWork/courseWork/main.c
@@ -1,5 +1,6 @@
 #include "allFunction.h"
 #include "recordDataBase.h"
+
     
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -12,7 +13,7 @@ int main() {
     FILE *fileDataBase;
     FILE *fileLogs;
     
-    arrayCompany = defaultRecordArray(arrayCompany);
+    company = defaultRecordArray(company);
     fileLogs = fopen("logs.txt", "a+t");
     
     titlePage();
@@ -21,43 +22,36 @@ int main() {
     clearScreen();
     
     // Записываем данные в файл
-    // НЕРАБОЧИЙ СПОСОБ(Разобраться)
-  /*  fileDataBase = fopen("dataBase.txt", "wb");
-            fwrite(&arrayCompany, sizeof(company), 1, fileDataBase);
-    fclose(fileDataBase); */
-    
-    // Рабочий способ записи данных по-умолчанию
-    fileDataBase = fopen("dataBase.txt", "wb");
-    for(i = 0; i < NUMBER_WORKSHOPS; i++){
-        fwrite(&arrayCompany.numberWorkshops[i], sizeof(arrayCompany.numberWorkshops[i]), 1, fileDataBase);
-    }
+    fileDataBase = fopen("dataBase.bin", "wb");
+            fwrite(&company, sizeof(company), 1, fileDataBase);
     fclose(fileDataBase);
+
     fprintf(fileLogs, "\n\n%s: Запись умалчиваемого массива структур в файл\n\n", ctime(&realTime));
     
     // Считываем информацию из файла в массив
-    fileDataBase = fopen("dataBase.txt", "rb");
-        fread(&arrayCompany, sizeof(company), 1, fileDataBase);
+    fileDataBase = fopen("dataBase.bin", "rb");
+        fread(&company, sizeof(companyStructure), 1, fileDataBase);
     fclose(fileDataBase);
     fprintf(fileLogs, "\n\n%s: Чтение умалчиваемого массива структур из файла\n\n", ctime(&realTime));
  
     // Вывод фио, отчисления в фонд
     printf("\n\n\t\t\tВывод отчислений в фонд\n");
-    for(i = 0; i < NUMBER_WORKSHOPS; i++){
-        for (j = 0; j < NUMBER_WORKER; j++) {
+    for(i = 0; i < company.countNumberWorkshops; i++){
+        for (j = 0; j < company.numberWorkshops[i].countNumberWorker; j++) {
         printf("\n=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+=+=+=+=+\n");
-        printf("Название цеха: %s\n", arrayCompany.numberWorkshops[i].nameWorkshop);
-        printf("ФИО Рабочего: %s\n", arrayCompany.numberWorkshops[i].numberWorker[j].surnameNamePatronymic);
-        printf("Сумма перечисленная в фонд мира: %.3f грн.\n", (arrayCompany.numberWorkshops[i].numberWorker[j].money[0]) / (arrayCompany.numberWorkshops[i].numberWorker[j].numberOfWorkDay[0]));
-        arrayFond[j] = (arrayCompany.numberWorkshops[i].numberWorker[j].money[0]) / (arrayCompany.numberWorkshops[i].numberWorker[j].numberOfWorkDay[0]);
+        printf("Название цеха: %s\n", company.numberWorkshops[i].nameWorkshop);
+        printf("ФИО Рабочего: %s\n", company.numberWorkshops[i].numberWorker[j].surnameNamePatronymic);
+        printf("Сумма перечисленная в фонд мира: %.3f грн.\n", (company.numberWorkshops[i].numberWorker[j].money[0]) / (company.numberWorkshops[i].numberWorker[j].numberOfWorkDay[0]));
+        arrayFond[j] = (company.numberWorkshops[i].numberWorker[j].money[0]) / (company.numberWorkshops[i].numberWorker[j].numberOfWorkDay[0]);
         }
     }
     fprintf(fileLogs, "\n\n%s: Вывод отчислений в фонд\n\n", ctime(&realTime));
     
     // Записываем фио, фондовые отчисления во второй файл
-    fileDataBase = fopen("twoDataBase.txt", "wb");
-    for(i = 0; i < NUMBER_WORKSHOPS; i++){
-        for(j = 0; j < NUMBER_WORKER; j++){
-            fwrite(&arrayCompany.numberWorkshops[i].nameWorkshop, sizeof(char), 1, fileDataBase);
+    fileDataBase = fopen("twoDataBase.bin", "wb");
+    for(i = 0; i < company.countNumberWorkshops; i++){
+        for(j = 0; j < company.numberWorkshops[i].countNumberWorker; j++){
+            fwrite(&company.numberWorkshops[i].nameWorkshop, sizeof(char), 1, fileDataBase);
             fwrite(&arrayFond[j], sizeof(float), 1, fileDataBase);
         }
     }
@@ -65,10 +59,10 @@ int main() {
     fprintf(fileLogs, "\n\n%s: Запись данных о фондовых отчислениях в файл\n\n", ctime(&realTime));
     
     // Считываем второй файл в массив
-    fileDataBase = fopen("twoDataBase.txt", "rb");
-    for(i = 0; i < NUMBER_WORKSHOPS; i++){
-        for(j = 0; j < NUMBER_WORKER; j++){
-            fread(&arrayCompany.numberWorkshops[i].nameWorkshop, sizeof(char), 1, fileDataBase);
+    fileDataBase = fopen("twoDataBase.bin", "rb");
+    for(i = 0; i < company.countNumberWorkshops; i++){
+        for(j = 0; j < company.numberWorkshops[i].countNumberWorker; j++){
+            fread(&company.numberWorkshops[i].nameWorkshop, sizeof(char), 1, fileDataBase);
             fread(&arrayFond[j], sizeof(float), 1, fileDataBase);
         }
     }
@@ -77,11 +71,11 @@ int main() {
     
     // Вывод второго файла
     printf("\n\t\t\tВывод считаной информации из второго файла\n");
-    for(i = 0; i < NUMBER_WORKSHOPS; i++){
-        for (j = 0; j < NUMBER_WORKER; j++) {
+    for(i = 0; i < company.countNumberWorkshops; i++){
+        for (j = 0; j < company.numberWorkshops[i].countNumberWorker; j++) {
         printf("\n=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+=+=+=+=+\n");
-        printf("Название цеха: %s\n", arrayCompany.numberWorkshops[i].nameWorkshop);
-        printf("ФИО Рабочего: %s\n", arrayCompany.numberWorkshops[i].numberWorker[j].surnameNamePatronymic);
+        printf("Название цеха: %s\n", company.numberWorkshops[i].nameWorkshop);
+        printf("ФИО Рабочего: %s\n", company.numberWorkshops[i].numberWorker[j].surnameNamePatronymic);
         printf("Сумма перечисленная в фонд мира: %.3f грн.\n", arrayFond[j]);
         }
     }
@@ -90,13 +84,14 @@ int main() {
     do{
         printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
         printf("\t\tДоступные действия:\n"
+               "%d - вывод всех записей на экран\n"
                "%d - добавление записи\n"
                "%d - удаление записей по наименованию цеха\n"
-               "%d - вывод всех записей на экран\n"
                "%d - создание бэкап-файла\n"
                "%d - восстановить базу данных из бэкап-файла\n"
+               "%d - корректировка цеха(изменение кол-ва работников)\n"
                "%d - выход из программы\n",
-               MENU_ADD, MENU_DELETE, MENU_SHOW_ALL, MENU_CREATE_BACKUP_FILE, MENU_RESTORE_BACKUP_FILE,MENU_EXIT);
+               MENU_SHOW_ALL, MENU_ADD, MENU_DELETE, MENU_CREATE_BACKUP_FILE, MENU_RESTORE_BACKUP_FILE, MENU_CORRECT_NUMBER_WORKER, MENU_EXIT);
         printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
         printf("Выберите действие: ");
         scanf("%d", &action);
@@ -106,23 +101,27 @@ int main() {
         switch(action){
             case MENU_ADD:
                 clearScreen();
-                countNewRecordWorkshops = addNewRecord(countNewRecordWorkshops, arrayCompany);
+                company.countNumberWorkshops = addNewRecord(company.countNumberWorkshops, company);
                 break;
             case MENU_DELETE:
                 clearScreen();
-                countNewRecordWorkshops = deleteRecords(countNewRecordWorkshops, arrayCompany);
+                company.countNumberWorkshops = deleteRecords(company.countNumberWorkshops, company);
                 break;
             case MENU_SHOW_ALL:
                 clearScreen();
-                printDataBase(countNewRecordWorkshops, arrayCompany);
+                printDataBase(company.countNumberWorkshops, company);
                 break;
             case MENU_CREATE_BACKUP_FILE:
                 clearScreen();
-                countRestore = createBackupFile(countNewRecordWorkshops, arrayCompany);
+                countRestore = createBackupFile(company.countNumberWorkshops, company);
                 break;
             case MENU_RESTORE_BACKUP_FILE:
                 clearScreen();
-                countNewRecordWorkshops = restoreBackupFile(countRestore, countNewRecordWorkshops);
+                company.countNumberWorkshops = restoreBackupFile(countRestore, company.countNumberWorkshops);
+                break;
+            case MENU_CORRECT_NUMBER_WORKER:
+                clearScreen();
+                company = correctCountWorker(company.countNumberWorkshops, company);
                 break;
             case MENU_EXIT:
                 fprintf(fileLogs, "\n\n%s: Завершение программы\n\n", ctime(&realTime));
@@ -136,3 +135,4 @@ int main() {
     fclose(fileLogs);
     return 0;
 }
+
